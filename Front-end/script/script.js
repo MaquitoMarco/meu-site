ed624dba6fddcec8a9b50aa71fea63f27f812123
MaquitoMarco <169164469+MaquitoMarco@users.noreply.github.com>
2024-07-27 17:50:50 -0300

    Update 27/07
    Inputs e botões para adicionar valor aos inputs

--- Front-end/script/script.js
+++ Front-end/script/script.js
@@ -1,35 +1,3 @@
-function manda() {
-    let bodyType = document.getElementById('bodyType');
-    let jogadorBT = bodyType.value;
-    let desabilitarEnviar = document.getElementById('btnManda');
-    let calcHp = document.getElementById('hpAtual');
-    let calcHpMax = document.getElementById('hpMaximo');
-    let testCon = document.getElementById('constituicao');
-
-    let bodyTypes = {
-        'Mago': [5, 10, 7, 8, 15, 13, 11, 6],
-        'Tank': [7, 5, 6, 13, 8, 11, 10, 15],
-        'Rogue': [8, 15, 13, 10, 5, 6, 11, 7],
-        'Warrior': [15, 8, 10, 11, 5, 6, 7, 13],
-        'Controller': [5, 6, 7, 8, 13, 11, 15, 10],
-        'Paladino': [10, 5, 6, 7, 13, 15, 8, 11],
-    };
-
-    document.getElementById('forca').value = bodyTypes[jogadorBT][0];
-    document.getElementById('destreza').value = bodyTypes[jogadorBT][1];
-    document.getElementById('agilidade').value = bodyTypes[jogadorBT][2];
-    document.getElementById('stamina').value = bodyTypes[jogadorBT][3];
-    document.getElementById('inteligencia').value = bodyTypes[jogadorBT][4];
-    document.getElementById('sabedoria').value = bodyTypes[jogadorBT][5];
-    document.getElementById('carisma').value = bodyTypes[jogadorBT][6];
-    document.getElementById('constituicao').value = bodyTypes[jogadorBT][7];
-
-    bodyType.disabled = true; // Desabilita o input de Bodytype
-    desabilitarEnviar.disabled = true;
-    calcHp.value = testCon.value * 2;
-    calcHpMax.value = calcHp.value;
-}
-
     let valoresAntigos = {};
     const $$ = seletor => document.querySelector(seletor)
 
@@ -83,8 +51,94 @@ document.addEventListener("DOMContentLoaded", function() {
     inputs.forEach(input => input.addEventListener('input', atualizarPontos)); // Garantir que o DOM esteja carregado.
 }); 
 
-document.getElementById('constituicao').onchange = function(campo) {
-    let hp = parseInt(this.value) * 2;
-    document.getElementById('hpAtual').value = hp;
-    document.getElementById('hpMaximo').value = hp;
-  }
+document.getElementById('bodyType').onchange = function manda(test) {
+    let bodyType = document.getElementById('bodyType');
+    let jogadorBT = bodyType.value;
+    let calcHp = document.getElementById('hpAtual');
+    let calcHpMax = document.getElementById('hpMaximo');
+    let calcCon = document.getElementById('constituicao');
+    let staminaCalc = document.getElementById('stamina');
+    let mpCalc = document.getElementById('mp');
+    let mpMax = document.getElementById('mpMax');
+
+    let bodyTypes = {
+        'Mago': [5, 10, 7, 11, 15, 13, 8, 6],
+        'Tank': [7, 5, 6, 13, 8, 11, 10, 15],
+        'Rogue': [7, 15, 13, 10, 5, 6, 11, 8],
+        'Warrior': [15, 8, 11, 10, 5, 6, 7, 13],
+        'Controller': [5, 6, 7, 8, 13, 11, 15, 10],
+        'Paladino': [10, 5, 6, 7, 13, 15, 8, 11],
+    };
+
+    document.getElementById('forca').value = bodyTypes[jogadorBT][0];
+    document.getElementById('destreza').value = bodyTypes[jogadorBT][1];
+    document.getElementById('agilidade').value = bodyTypes[jogadorBT][2];
+    document.getElementById('stamina').value = bodyTypes[jogadorBT][3];
+    document.getElementById('inteligencia').value = bodyTypes[jogadorBT][4];
+    document.getElementById('sabedoria').value = bodyTypes[jogadorBT][5];
+    document.getElementById('carisma').value = bodyTypes[jogadorBT][6];
+    document.getElementById('constituicao').value = bodyTypes[jogadorBT][7];
+
+    mpCalc.value = staminaCalc.value * 2;
+    mpMax.value = mpCalc.value;
+    calcHp.value = calcCon.value * 2;
+    calcHpMax.value = calcHp.value;
+    bodyType.disabled = true; // Desabilita o input de Bodytype
+}
+
+document.getElementById('pontosDistribuir').addEventListener('input', function() {
+    let test2 = document.getElementById('pontosDistribuir');
+    let pedrada = parseInt(test2.value);
+
+    if (pedrada <= 0) {
+        let inputs = document.querySelectorAll('.status');
+        inputs.forEach(input => input.disabled = true);
+    }
+});
+
+// Botões de adicionar um valor a um input, e calcular se necessario.
+
+function atualizaInput(idInput, valor) {
+    let num = document.getElementById(idInput);
+    let tiraPontos = document.getElementById('pontosDistribuir');
+
+    num.value = parseInt(num.value) + valor;
+    tiraPontos.value = parseInt(tiraPontos.value) - 1;
+
+    let event = new Event('input');
+    tiraPontos.dispatchEvent(event);
+}
+
+function atualizaCon(idInput1, valor) {
+    let num = document.getElementById(idInput1);
+    let tiraPontos = document.getElementById('pontosDistribuir');
+    let calcHp = document.getElementById('hpAtual');
+    let calcHpMax = document.getElementById('hpMaximo');
+    let hp = (parseInt(num.value) + valor) * 2;
+
+    num.value = parseInt(num.value) + valor;
+    calcHp.value = hp;
+    calcHpMax.value = hp;
+
+    tiraPontos.value = parseInt(tiraPontos.value) - 1;
+
+    let event = new Event('input');
+    tiraPontos.dispatchEvent(event);
+}
+
+function atualizaSta(idInput1, valor) {
+    let num = document.getElementById(idInput1);
+    let tiraPontos = document.getElementById('pontosDistribuir');
+    let calcMp = document.getElementById('mp');
+    let calcmpMax = document.getElementById('mpMax');
+    let mp = (parseInt(num.value) + valor) * 2;
+
+    num.value = parseInt(num.value) + valor;
+    calcMp.value = mp;
+    calcmpMax.value = mp;
+
+    tiraPontos.value = parseInt(tiraPontos.value) - 1;
+
+    let event = new Event('input');
+    tiraPontos.dispatchEvent(event);
+}
